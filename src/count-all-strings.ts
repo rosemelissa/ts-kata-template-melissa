c64323cd3a4d9fa228ec2ae1e723fc87bdb58aff
Melissa Rose <123melissa321@gmail.com>
2022-07-13 09:50:33 +0100

    Added jest tests for count-all-strings

---
+++ src/count-all-strings.ts
@@ -0,0 +1,20 @@
+/*
+Return the count of all strings in an array of numbers and strings.  Use array functions, not a loop. (TypeScript)
+
+Use typescript, ideally.
+use array functions - don’t use a loop
+
+Example
+=======
+countStrings([**”old”**, 10, 20, **“pond”**, -5, -3]) should return 2
+
+PSEUDOCODE:
+
+make a new array called STRINGARRAY by filtering INPUTARRAY by type to remove all numbers and keep strings
+return the length of STRINGARRAY
+
+*/
+
+function countAllStrings(inputArray: any[]): number {}
+
+export default countAllStrings;
